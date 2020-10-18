675e82ca457333c187d0af71bef87383dbc5274e
Lukas Köller <lukaskoeller@Lukass-MacBook-Pro.fritz.box>
2020-10-18 23:26:16 +0200

    feat(Home): Welcome text & bottom nav with new & join game

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>Caramba! Welcome to perfect poster 👋</h1>
+    <Card>
+      <h2>🧑‍🎨 Card Component</h2>
+    </Card>
+    <BottomNav>
+      <BottomButton to="/join">Join Game</BottomButton>
+      <BottomButton to="/new">New Game</BottomButton>
+    </BottomNav>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Card from '@/components/Card.vue'; // @ is an alias to /src
+import BottomNav from '@/components/BottomNav.vue';
+import BottomButton from '@/components/BottomButton.vue';
 
 export default defineComponent({
   name: 'Home',
   components: {
-    HelloWorld,
+    Card,
+    BottomNav,
+    BottomButton,
   },
 });
 </script>
+
+<style scoped>
+  h1 {
+    text-align: center;
+  }
+</style>
