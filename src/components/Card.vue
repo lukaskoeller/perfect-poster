fff61e020cbaf895d58bf1d8e46b60becc528221
Lukas Köller <lukaskoeller@Lukass-MacBook-Pro.fritz.box>
2020-10-18 23:25:33 +0200

    feat(Card): New card component

---
+++ src/components/Card.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Card',
+  //   props: {
+  //     msg: String,
+  //   },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.card {
+  width: min(600px, 100%);
+  padding: var(--spacing-5);
+  background-color: var(--primary-color-lighten-2);
+  border-radius: var(--card-radius);
+}
+</style>
