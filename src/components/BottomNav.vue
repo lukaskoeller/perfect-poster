1c091ffd114be8815c8d2c862b7715520083eeb0
Lukas Köller <lukaskoeller@Lukass-MacBook-Pro.fritz.box>
2020-10-18 23:25:21 +0200

    feat(BottomNav): New buttom nav component

---
+++ src/components/BottomNav.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="bottom-nav">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'BottomNav',
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.bottom-nav {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  /* padding: var(--spacing-4); */
+
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(20px, 1fr));
+  grid-gap: 1px;
+}
+</style>
