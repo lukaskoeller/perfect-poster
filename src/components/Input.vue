c8b48cd5abdff85c840209867df69663b8f09ca1
Lukas Köller <lukaskoeller@Lukass-MacBook-Pro.fritz.box>
2020-10-18 23:25:47 +0200

    feat(Input): New Input component

---
+++ src/components/Input.vue
@@ -0,0 +1,26 @@
+<template>
+  <input class="input" :type="type" :placeholder="placeholder" :value="value" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Input',
+  props: {
+    type: String,
+    placeholder: String,
+    value: {
+      type: String,
+      default: '',
+    },
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.input {
+  background-color: var(--primary-color-lighten-2);
+}
+</style>
