9e20fa7bb25176ce6a192cad8108bfdc954996e5
Lukas Köller <lukaskoeller@Lukass-MacBook-Pro.fritz.box>
2020-10-18 23:26:33 +0200

    feat(New): New Game page. Used to host & start game

---
+++ src/views/New.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="new-game">
+    <h1>Give your Game a name!</h1>
+    <Input type="text" placeholder="Name of the Game..." />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Input from '@/components/Input.vue';
+
+export default defineComponent({
+  name: 'New',
+  components: {
+    Input,
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
