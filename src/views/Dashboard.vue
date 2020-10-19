179be0c83e9ce3b216cfbb05ea4bc7c13db25421
lukaskoeller <lukas-koeller@onlinehome.de>
2020-10-19 23:53:04 +0200

    feat(Dashboard): New component. Handles player list & other

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="new-game">
+    <h1>Wait for players.</h1>
+    <GameLink></GameLink>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import GameLink from '@/components/GameLink.vue';
+
+export default defineComponent({
+  name: 'Dasboard',
+  components: {
+    GameLink,
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
