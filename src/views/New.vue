6014890daf2f3963ddf80cf209eb3a598547cdbc
lukaskoeller <lukas-koeller@onlinehome.de>
2020-10-19 23:53:25 +0200

    feat(New): Add BottomNav component (back/continue)

--- src/views/New.vue
+++ src/views/New.vue
@@ -2,17 +2,25 @@
   <div class="new-game">
     <h1>Give your Game a name!</h1>
     <Input type="text" placeholder="Name of the Game..." />
+    <BottomNav>
+      <BottomButton to="/">Back</BottomButton>
+      <BottomButton to="/dashboard">Continue</BottomButton>
+    </BottomNav>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import Input from '@/components/Input.vue';
+import BottomNav from '@/components/BottomNav.vue';
+import BottomButton from '@/components/BottomButton.vue';
 
 export default defineComponent({
   name: 'New',
   components: {
     Input,
+    BottomNav,
+    BottomButton,
   },
 });
 </script>
