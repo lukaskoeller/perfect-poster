b0589de25002eaa119d61015f55daedb3aa4cb4e
lukaskoeller <lukas-koeller@onlinehome.de>
2020-12-13 18:32:41 +0100

    chore(backend): setup basic express server

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,19 @@
-console.log("helly, I'm backend.");
+console.log("Welcome to perfect poster!");
+
+import cors from 'cors';
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+
+app.get('/newgame', (req, res) => {
+    console.log('Send all services');
+});
+
+app.listen(port, () => {
+    console.log(`perfect poster listening at http://localhost:${port}`);
+});
