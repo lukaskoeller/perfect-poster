5040e106d2cfd76aa6cfb75e975b346ac322337f
lukaskoeller <lukas-koeller@onlinehome.de>
2020-12-14 20:02:44 +0100

    feat(backend): new fn newSession, newGame

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,53 @@
 import WebSocket from 'ws';
+import { v4 as uuidv4 } from 'uuid';
 
 const PORT = 8081;
 
 const wss = new WebSocket.Server({ port: PORT });
 
+let sessions = [];
+
+/**
+ * newGame
+ * @param {string} sessionId
+ */
+const newGame = (sessionId) => {
+  const gameId = uuidv4();
+
+  console.log(`Game ID: ${gameId}\nSession ID: ${sessionId}`);
+
+  return {
+    gameId,
+    players: [],
+  }
+};
+
+/**
+ * newSession
+ */
+const newSession = () => {
+  const sessionId = uuidv4();
+
+  const session = {
+    sessionId,
+    games: newGame(sessionId),
+  };
+
+  sessions.push(session);
+  return session;
+};
+
 wss.on('connection', function connection(ws) {
   console.log(`Connected to websocket.`);
 
   ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
+    switch (message) {
+      case 'newSession':
+        ws.send(JSON.stringify(newSession()));
+        break;
+    
+      default:
+        break;
+    }
   });
-
-  ws.send('something');
 });
